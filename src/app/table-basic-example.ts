36e70bfcafb5aa5e7415e6fffcfca0c3606482de
arinjoyb7 <arinjoyb7@gmail.com>
2020-08-10 13:21:19 +0200

    2 files modified

--- src/app/table-basic-example.ts
+++ src/app/table-basic-example.ts
@@ -2,23 +2,26 @@ import {Component} from '@angular/core';
 
 export interface PeriodicElement {
   name: string;
-  position: number;  
+  position: string;  
   transit: string;
-  cost: number;
+  cost: string;
+  lhcost: string;
+  accost: string;
   mode : string;
   equipment : string;
+  distance : string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-     {position: 1, name: 'SAIA Motor Freight', equipment: 'Dry Van', cost: 508.70, transit: '2 Days' , mode: 'Parcel'},
-  {position: 2, name: 'UPS Freight LTL', equipment: 'Flat Bed',cost: 518.30, transit: '2 Days',mode: 'LTL'},
-  {position: 3, name: 'R+L Carriers', equipment: 'Refrigerated',cost: 602.10, transit: '2 Days',mode: 'LTL'},
-  {position: 4, name: 'New Penn',equipment: 'Flat Bed',cost: 618.45, transit: '2 Days',mode: 'TL'},
-  {position: 5, name: 'XLS Logistics',equipment: 'Dry Van', cost: 618.70, transit: '2 Days',mode: 'Parcel'},
-  {position: 6, name: 'YRC Freight',equipment: 'Dry Van', cost: 623.67, transit: '2 Days',mode: 'Parcel'},
-  {position: 7, name: 'DHL Freight',equipment: 'Flat Bed', cost: 648.56, transit: '2 Days',mode: 'LTL'},
-  {position: 8, name: 'DHL Express',equipment: 'Refrigerated', cost: 845.70, transit: '1 Day',mode: 'LTL'},
-  {position: 9, name: 'Toll Road Express',equipment: 'Dry Van', cost: 908.70, transit: '1 Day',mode: 'LTL'},
+     {position: 'Yes', name: 'SAIA Motor Freight', equipment: 'Dry Van', cost: '508.70 $', lhcost: '350 $',accost:'158.70 $',distance: '350 miles',transit: '2 Days' , mode: 'Parcel'},
+  {position: 'Yes', name: 'UPS Freight LTL', equipment: 'Flat Bed',cost: '518.30 $',  lhcost: '360 $',accost:'158.30 $',distance: '350 miles',transit: '2 Days',mode: 'LTL'},
+  {position: '', name: 'R+L Carriers', equipment: 'Refrigerated',cost: '602.10', lhcost: '400 $',accost:'202.10 $',distance: '350 miles', transit: '2 Days',mode: 'LTL'},
+  {position: '', name: 'New Penn',equipment: 'Flat Bed',cost: '618.45 $', lhcost: '410 $',accost:'208.45 $',distance: '350 miles', transit: '2 Days',mode: 'TL'},
+  {position: '', name: 'XLS Logistics',equipment: 'Dry Van', cost: '618.70 $', lhcost: '410 $',accost:'208.70 $',distance: '350 miles', transit: '2 Days',mode: 'Parcel'},
+  {position: '', name: 'YRC Freight',equipment: 'Dry Van', cost: '623.67 $', lhcost: '410 $',accost:'213.60 $',distance: '350 miles', transit: '2 Days',mode: 'Parcel'},
+  {position: '', name: 'DHL Freight',equipment: 'Flat Bed', cost: '648.56 $', lhcost: '450 $',accost:'218.56 $',distance: '350 miles', transit: '2 Days',mode: 'LTL'},
+  {position: '', name: 'DHL Express',equipment: 'Refrigerated', cost:'845.70 $', lhcost: '640 $',accost:'205.70 $',distance: '350 miles', transit: '1 Day',mode: 'LTL'},
+  {position: '', name: 'Toll Road Express',equipment: 'Dry Van', cost:'908.70 $', lhcost: '650 $',accost:'258.70 $',distance: '350 miles', transit: '1 Day',mode: 'LTL'},
 ];
 
 /**
@@ -30,7 +33,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'table-basic-example.html',
 })
 export class TableBasicExample {
-  displayedColumns: string[] = ['position', 'carrier', 'mode', 'equipment','transit','cost'];
+  displayedColumns: string[] = ['position', 'carrier', 'mode', 'equipment','transit','distance','cost','lhcost','accost'];
   dataSource = ELEMENT_DATA;
 }
 
